cbfd95f65ef1f07be47e17c50b63f86203a2d7a5
rajmohang111@gmail.com <37865180+aswany-das@users.noreply.github.com>
2018-04-02 16:58:07 +0530

    add files

---
+++ bin/README.md
@@ -0,0 +1,5 @@
+# spring-boot-todo-app
+
+**Run the webservices using STS IDE**
+
+The backend server will start at <http://localhost:8080>.
--- src/main/java/com/example/demo/todo/TodoController.java
+++ src/main/java/com/example/demo/todo/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +17,27 @@ public class TodoController {
 	@Autowired
 	private TodoServices todoservice;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/todos")
 	public List<Todo> getTodoList() {
 		
 		return todoservice.getAllTodos();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/todos/{id}")
 	public Optional<Todo> getTodo(@PathVariable Integer id) {
 
 		return todoservice.getTodo(id);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method=RequestMethod.POST, value="todos")
 	public List<Todo> addTodo(@RequestBody Todo todo) {
 		return todoservice.addTodo(todo);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(method=RequestMethod.DELETE, value="todos/{id}")
 	public List<Todo> addTodo(@PathVariable Integer id) {
 		return todoservice.deleteTodo(id);
